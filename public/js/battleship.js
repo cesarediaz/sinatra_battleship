ee319017f7578a8b1d46a26580b5663ba69cf557
Cesar Diaz <cesare.diaz@gmail.com>
2016-03-31 17:54:16 -0300

    refactorized battleship script

--- public/js/battleship.js
+++ public/js/battleship.js
@@ -1,58 +1,26 @@
-$( document ).ready(function() {
-    $(function(){
-      var hash = {};
-      Object.keys(hash).forEach(function (key) {
-        console.log(hash[key]);
-      })
-    });
+var Battleship = function (){};
 
-    $('#opponent').find('.square').on('click', function(){
-      var id = this.id;
-
-      if (id != '') {
-        $(this).attr('style', 'background-color: red');
-
-        $.ajax({
-          type: "POST",
-          url: '/shot',
-          data: {id: id},
-          success: function(data) {
-            var coords = data['shot_coordinates'];
-            var ship_data = $('.mine').find(jq(coords)).attr('class').match(/\d+/);
-            $('.mine').find(jq(coords)).attr('style', 'background-color: blue');
-            destroy_ship(ship_data);
-            message(data);
-          },
-          dataType: 'json'
-        });
-
-      }
-
-    })
-});
-
-function jq(id) {
+Battleship.prototype.jq = function(id) {
   return "#" + id.replace( /(:|\.|\[|\]|,)/g, "\\$1" );
-}
+};
 
-function destroy_ship(ship_data) {
+Battleship.prototype.destroy_ship = function(ship_data) {
   if (ship_data != null) {
     var klass = '.' + ship_data[0];
     $('.mine').find(klass).attr('style', 'background-color: gray; color: gray;');
   }
-}
+};
 
-function message(data) {
-  if (data['result'] === 'winner') {
-    modal(BootstrapDialog.TYPE_SUCCESS, 'winner');
+Battleship.prototype.message = function(data) {
+  var result = data['result'];
+  var result_types = {winner: BootstrapDialog.TYPE_SUCCESS, loser: BootstrapDialog.TYPE_DANGER};
+  if (result != '') {
+    this.modal(result_types[result], result);
+    this.autoclose_modal(2000);
   }
-  if (data['result'] === 'loser') {
-    modal(BootstrapDialog.TYPE_DANGER, 'loser');
-  }
-}
+};
 
-
-function modal(type, result){
+Battleship.prototype.modal = function(type, result) {
   BootstrapDialog.show({
     type: type,
     title: 'Result: ',
@@ -63,6 +31,35 @@ function modal(type, result){
                   dialogRef.close();
                   location.reload();
               }
-          }]
+            }]
   });
-}
+};
+
+Battleship.prototype.autoclose_modal = function(time = 5000) {
+  setTimeout(function(){
+    $('.modal').modal('hide');
+  }, time);
+};
+
+$( document ).ready(function() {
+  $('#opponent').find('.square').on('click', function(){
+    var id = this.id;
+    if (id != '') {
+      $(this).attr('style', 'background-color: red');
+      $.ajax({
+        type: "POST",
+        url: '/shot',
+        data: {id: id},
+        success: function(data) {
+          var battleship = new Battleship();
+          var coords = data['shot_coordinates'];
+          var ship_data = $('.mine').find(battleship.jq(coords)).attr('class').match(/\d+/);
+          $('.mine').find(battleship.jq(coords)).attr('style', 'background-color: blue');
+          battleship.destroy_ship(ship_data);
+          battleship.message(data);
+        },
+        dataType: 'json'
+      });
+    }
+  });
+});
